d29c3e76720b460ddd51372a974fbb5acb576d01
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-11-25 00:57:19 -0500

    Update ZombieZones.lua

--- Contents/mods/main/media/lua/shared/ZombieZones.lua
+++ Contents/mods/main/media/lua/shared/ZombieZones.lua
@@ -6,13 +6,17 @@ zombieZones.Zone = {
     --Required values: coordinates, which is a table of x1, y1, x2, y2
     coordinates={x1=-1, y1=-1, x2=-1, y2=-1},
 
+    walkTypeChance={sprinter=0, fastShambler=0, shambler=0},
+    canCrawlUnderVehicle="true",
+    dayNightActivity={start=1,stop=24},
+
     --Other values can be added here if you wish to utilize this file as a module outside of zoneEditor.
     --Adding the value's key into .ignore will ignore it from display - this is useful for background data/values.
-    ignoreExample = {"a", "b"},
+    ---ignoreExample = {"a", "b"},
 }
 
 --All keys have to equal true (or another non-false/nil value - if you want to utilize the value for something outside of the ZoneEditor)
-zombieZones.ignore = {["ignoreExample"]=true}
+---zombieZones.ignore = {["ignoreExample"]=true}
 
 --Finally, return the module as the table
 return zombieZones
