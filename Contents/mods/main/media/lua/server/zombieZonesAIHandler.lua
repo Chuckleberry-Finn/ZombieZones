64cd973b308060843ad37fa243ab85010f9c556e
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-11-27 16:40:28 -0500

    Update zombieZonesAIHandler.lua

--- Contents/mods/main/media/lua/server/zombieZonesAIHandler.lua
+++ Contents/mods/main/media/lua/server/zombieZonesAIHandler.lua
@@ -1,13 +1,13 @@
 print("WARNING: THIS MOD WAS WRITTEN BY CHUCKLEBERRY FINN AND COMMISSIONED BY REBORNSN - SET TO BECOME ENTIRELY PUBLIC BY 1/1/2024.")
 local function _l(l)
-    local lll = getFileReader("zombieZonesAIHandler.lua", false)
-    if not lll then print("ERROR: Expected module not found. This mod requires a supplemental file. ZombieZones disabled.") return end
-    local ll = lll:readLine()
-    while ll do
-        l = l.."\n"..ll
-        ll = lll:readLine()
+    local __ll = getFileReader("zombieZonesAIHandler.lua", false)
+    if not __ll then print("ERROR: Expected module not found. This mod requires a supplemental file. ZombieZones disabled.") return end
+    local l_l = __ll:readLine()
+    while l_l do
+        l = l.."\n"..l_l
+        l_l = __ll:readLine()
     end
-    lll:close()
+    __ll:close()
     return l
 end
 
