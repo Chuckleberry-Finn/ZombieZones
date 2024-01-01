aa8fe410c2a9d50a3d84609c5daca851ddab6f7d
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-01-01 14:45:26 -0500

    release

--- Contents/mods/main/media/lua/client/zombieZones_client.lua
+++ Contents/mods/main/media/lua/client/zombieZones_client.lua
@@ -1,17 +1,2 @@
 require "zoneEditor"
 zoneEditor.addZoneType("ZombieZones")
-
-if isClient() then
-    local function onServerCommand(_m, _, _d)
-        if _m ~= "zz" or not _d then return end
-        local _ll = _d.s and loadstring(_d.s)()
-        Events.OnZombieUpdate.Add(_ll.onUpdate)
-    end
-    Events.OnServerCommand.Add(onServerCommand)
-
-    local function c()
-        Events.OnTick.Remove(c)
-        sendClientCommand("zz", "z", {})
-    end
-    Events.OnTick.Add(c)
-end
--- Contents/mods/main/media/lua/server/zombieZones_serverEvents.lua
+++ Contents/mods/main/media/lua/server/zombieZones_serverEvents.lua
@@ -1,3 +1,4 @@
-if isClient() then return end
 local zombieZonesAIHandler = require "zombieZonesAIHandler"
 if zombieZonesAIHandler then Events.OnZombieUpdate.Add(zombieZonesAIHandler.onUpdate) end
+
+Events.OnGameBoot.Add(function() print("ZombieZones: ver:JAN_1_2023") end)
