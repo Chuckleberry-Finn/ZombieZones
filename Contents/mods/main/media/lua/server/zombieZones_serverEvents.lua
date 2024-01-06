460212afc0aa2dabca3abc00fd0c29160f0f7015
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-01-05 22:55:12 -0500

    Update zombieZones_serverEvents.lua

--- Contents/mods/main/media/lua/server/zombieZones_serverEvents.lua
+++ Contents/mods/main/media/lua/server/zombieZones_serverEvents.lua
@@ -1,4 +1,4 @@
 local zombieZonesAIHandler = require "zombieZonesAIHandler"
 if zombieZonesAIHandler then Events.OnZombieUpdate.Add(zombieZonesAIHandler.onUpdate) end
 
-Events.OnGameBoot.Add(function() print("ZombieZones: ver:JAN_1_2023") end)
+Events.OnGameBoot.Add(function() print("ZombieZones: ver:JAN_5_2023") end)
