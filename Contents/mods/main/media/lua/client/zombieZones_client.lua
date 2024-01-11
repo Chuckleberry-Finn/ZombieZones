ff186911e3eb15928ba69959c1a96a3a605a3a00
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-01-10 23:19:31 -0500

    fix

--- Contents/mods/main/media/lua/client/zombieZones_client.lua
+++ Contents/mods/main/media/lua/client/zombieZones_client.lua
@@ -1,2 +1,5 @@
 require "zoneEditor"
 zoneEditor.addZoneType("ZombieZones")
+
+local zombieZonesAIHandler = require "zombieZones_AIHandler"
+if zombieZonesAIHandler then Events.OnZombieUpdate.Add(zombieZonesAIHandler.onUpdate) end
--- Contents/mods/main/media/lua/server/zombieZones_serverEvents.lua
+++ Contents/mods/main/media/lua/server/zombieZones_serverEvents.lua
@@ -1,4 +1,1 @@
-local zombieZonesAIHandler = require "zombieZonesAIHandler"
-if zombieZonesAIHandler then Events.OnZombieUpdate.Add(zombieZonesAIHandler.onUpdate) end
-
 Events.OnGameBoot.Add(function() print("ZombieZones: ver:JAN_5_2023") end)
